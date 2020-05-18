5f3c422690b69ff2eb9678c5ceb548059c5f2378
Mirbek Askerov <mirbek.askerov@yandex.ru>
2020-05-19 02:18:08 +0600

    Rubocop fixes

--- api.rb
+++ api.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'bundler/setup'
 require 'cryptoexchange'
@@ -7,30 +8,28 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 
 get '/' do
-    client = Cryptoexchange::Client.new
-    result = Hash.new
-    result["pairs"] = Array.new 
-    pairs = client.pairs(params[:dce])
-    pairs.each {|p|
-        result['pairs'] << { 'symbol' => p.base + '-' +p.target }
-    }
-    result.to_json
+  client = Cryptoexchange::Client.new
+  result = {}
+  result['pairs'] = []
+  pairs = client.pairs(params[:dce])
+  pairs.each { |p| result['pairs'] << { 'symbol' => "#{p.base}-#{p.target}" } }
+  result.to_json
 end
 
 get '/ticker' do
-    client = Cryptoexchange::Client.new
-    result = Hash.new
-    result['ticker'] = Array.new
-    pair = Cryptoexchange::Models::MarketPair.new(base: params[:base], target: params[:target], market: params[:market])
-    ticker = client.ticker(pair)
-    result['ticker'] << {
-        'last' => ticker.last,
-        'bid' => ticker.bid,
-        'ask' => ticker.ask,
-        'high' => ticker.high,
-        'low' => ticker.low,
-        'change' => ticker.change,
-        'volume' => ticker.volume
-    }
-    result.to_json
-end    
+  client = Cryptoexchange::Client.new
+  result = {}
+  result['ticker'] = []
+  pair = Cryptoexchange::Models::MarketPair.new(base: params[:base], target: params[:target], market: params[:market])
+  ticker = client.ticker(pair)
+  result['ticker'] << {
+    'last' => ticker.last,
+    'bid' => ticker.bid,
+    'ask' => ticker.ask,
+    'high' => ticker.high,
+    'low' => ticker.low,
+    'change' => ticker.change,
+    'volume' => ticker.volume
+  }
+  result.to_json
+end

79d0ca35d88e2d5e855d3630074a0c10f4f056b8
Mirbek Askerov <mirbek.askerov@yandex.ru>
2020-05-19 02:30:58 +0600

    Refact code. Add exception & logger

--- api.rb
+++ api.rb
@@ -4,32 +4,39 @@
 require 'bundler/setup'
 require 'cryptoexchange'
 require 'sinatra'
+require 'logger'
 
 set :bind, '0.0.0.0'
 
+logger = Logger.new(STDOUT)
+
 get '/' do
-  client = Cryptoexchange::Client.new
-  result = {}
-  result['pairs'] = []
-  pairs = client.pairs(params[:dce])
-  pairs.each { |p| result['pairs'] << { 'symbol' => "#{p.base}-#{p.target}" } }
-  result.to_json
+  begin
+    result = { 'pairs' => [] }
+    pairs = Cryptoexchange::Client.new.pairs(params[:dce])
+  rescue StandardError => e
+    logger.error("#{e.message} #{e.class}\n")
+  else
+    pairs.each { |p| result['pairs'] << { 'symbol' => "#{p.base}-#{p.target}" } }
+    result.to_json
+  end
 end
 
 get '/ticker' do
-  client = Cryptoexchange::Client.new
-  result = {}
-  result['ticker'] = []
-  pair = Cryptoexchange::Models::MarketPair.new(base: params[:base], target: params[:target], market: params[:market])
-  ticker = client.ticker(pair)
-  result['ticker'] << {
-    'last' => ticker.last,
-    'bid' => ticker.bid,
-    'ask' => ticker.ask,
-    'high' => ticker.high,
-    'low' => ticker.low,
-    'change' => ticker.change,
-    'volume' => ticker.volume
-  }
-  result.to_json
+  begin
+    pair = Cryptoexchange::Models::MarketPair.new(base: params[:base], target: params[:target], market: params[:market])
+    ticker = Cryptoexchange::Client.new.ticker(pair)
+  rescue StandardError => e
+    logger.error("#{e.message} #{e.class}\n")
+  else
+    { 'ticker' => [] << {
+      'last' => ticker.last,
+      'bid' => ticker.bid,
+      'ask' => ticker.ask,
+      'high' => ticker.high,
+      'low' => ticker.low,
+      'change' => ticker.change,
+      'volume' => ticker.volume
+    } }.to_json
+  end
 end
